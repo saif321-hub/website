6ba79f2425ea3305a0fd97cd5d3533550fbbdeb6
Saif <saifresdmi@gmail.com>
2024-03-15 22:39:58 +0300

    trying to add button that add multi makers

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ let btn = document.getElementById('btn')
 let filed = document.getElementById('myText')
 let x = document.getElementById("myText").value;
 let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
 
 
 const db = getDatabase()
@@ -49,12 +50,23 @@ btn.addEventListener("click", (event) => {
 btn2.addEventListener('click', (event) => {
   unsubscribe()
   btn.disabled = false;
+  btn2.disabled = true;
   filed.value = '';
   map.removeLayer(marker)
   map.removeLayer(circle)
 })
 
+btn3.addEventListener('click', (event) => {
+  x = document.getElementById("myText").value;
+  refC = ref(db, `users/${x}`)
+  if (x == '') {
+    window.alert("can nor be empty");
+  }
+  else {
+    groupLive();
+  }
 
+})
 var map = L.map('map',
   {
     zoomControl: false,
@@ -107,4 +119,29 @@ function liveMark() {
   })
 }
 
+const idList = ['helo'];
+function groupLive() {
+  onValue(refC, (snapshot) => {
+    latCoord = Number(snapshot.val().lat)
+    longCoord = Number(snapshot.val().lang)
+    let devId = snapshot.val().ID;
+    for (let i = 0; i < idList.length; i++) {
+      if (idList[i] == devId) {
+        window.alert("already in track");
+        break;
+      }
+      else {
+        idList.push(devId);
+        console.log(devId);
+        marker = L.marker([latCoord, longCoord]).addTo(map).bindPopup(`<h1>${snapshot.val().ID}<h1/>`)
+        circle = L.circle([latCoord, longCoord], 3).addTo(map)
 
+        if (!zoomed) {
+          zoomed = map.fitBounds(circle.getBounds())
+        }
+      }
+    }
+
+    //map.setView([latCoord, longCoord])
+  })
+}
